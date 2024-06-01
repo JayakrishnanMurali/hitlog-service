5a08ebc36f4d87376fa74f801e2cfb2b38234c78
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-06-02 00:56:36 +0530

    refactor: Remove unnecessary date time logging in logger middleware

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -3,7 +3,6 @@ import chalk from "chalk";
 
 export const logger = morgan((tokens, req, res) => {
   return [
-    // date time in the format [2017-11-02T11:13:54.545]
     chalk.gray(tokens.date(req, res)),
     chalk.green.bold(tokens.method(req, res)),
     chalk.red.bold(tokens.status(req, res)),
